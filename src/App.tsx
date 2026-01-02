654115ab6f8a01c51c0f7ccadf2449d675fc56b2
Elisa Martinez <elisam.esquivel@gmail.com>
2026-01-02 12:03:39 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from '@/components/ui/button';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-800">Hola</h1>
+      <Button className="bg-amber-400">Click me</Button>
+    </>
+  );
+}
+
+export default App;
